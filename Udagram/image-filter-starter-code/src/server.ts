3fb71f2829884fc815857b1b4648f979a98b5fe4
kagozi <alexkagozi.com>
2022-12-28 14:03:20 +0300

    Recommended changes

--- Udagram/image-filter-starter-code/src/server.ts
+++ Udagram/image-filter-starter-code/src/server.ts
@@ -1,9 +1,7 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
-import { runInNewContext } from 'vm';
-import { read } from 'fs';
-import { pseudoRandomBytes } from 'crypto';
+import bodyParser from "body-parser";
+import express from "express";
+import { Request, Response } from "express";
+import { deleteLocalFiles, filterImageFromURL } from "./util/util";
 
 (async () => {
 
@@ -12,7 +10,7 @@ import { pseudoRandomBytes } from 'crypto';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -20,50 +18,53 @@ import { pseudoRandomBytes } from 'crypto';
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
+  // QUERY PARAMETERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
 
-  //! END @TODO1
-  
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    const { image_url } = req.query;
+    if (typeof image_url != 'string') {
+      throw Error("The url should be of type string")
+    }
+    if (!image_url) {
+      res.status(400).send("image_url query parameter is required");
+    }
+
+    try {
+      const filteredFile = await filterImageFromURL(image_url);
+
+      res.status(200).sendFile(filteredFile, () => {
+        deleteLocalFiles([filteredFile]);
+      });
+    } catch (error) {
+      res.status(422).send(`Unable to process ${image_url}`);
+      // tslint:disable-next-line: no-console
+      console.error(error);
+    }
+
+  });
+
+  // ! END @TODO1
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get("/", async (req, res) => {
-    
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-
-  app.get("/filteredImage", async(req:express.Request, res:express.Response) => {
-    const filteredImageUrl = req.query.image_url;
-
-    if (!filteredImageUrl) {
-      return res.status(400).send("Please enter a valid image url to proceed");
-    }
-    try {
-      const filteredContent = await filterImageFromURL(filteredImageUrl);
-      res.status(200).sendFile(filteredContent, ()=>{
-        deleteLocalFiles([filteredContent]);
-    });
-    } catch (error) {
-        res.status(422).send(`Error occurred when processing ${filteredImageUrl}`);
-        console.error(error);
-    }
-
-    
-    
+    res.send("try GET /filteredimage?image_url={{}}");
   });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    // tslint:disable-next-line: no-console
+    console.log(`server running http://localhost:${port}`);
+    // tslint:disable-next-line: no-console
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
