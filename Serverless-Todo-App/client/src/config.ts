09b31830548ca80722487b133abb612621253ea5
kagozi <alexkagozi.com>
2023-02-09 22:33:52 +0300

    Serverless ToDo App

---
+++ Serverless-Todo-App/client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 't4ijne3qth';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/staging`
+
+export const authConfig = {
+  domain: 'dev-2l5lrmmkji78x22r.us.auth0.com',
+  clientId: 'e6b0lVxl6kvxbOmmhXokijScI50kNTnW',
+  callbackUrl: 'http://localhost:3000/callback'
+};
